f2d11f97763575724da5a287b7baabc6b8c9dd52
fermelli <luisfernandosalgadomiguez@gmail.com>
2021-11-02 03:18:37 -0400

    agregar campo descuento a tabla promociones

--- tablapromociones.sql
+++ tablapromociones.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS promociones (
     id INT NOT NULL AUTO_INCREMENT,
     nombre VARCHAR(72) NOT NULL,
+    descuento INT NOT NULL CHECK(descuento >= 1 AND descuento <= 100),
     fecha DATE NOT NULL DEFAULT NOW(),
     fecha_inicio DATE NOT NULL CHECK(fecha_inicio >= fecha),
     fecha_fin DATE NOT NULL CHECK(fecha_fin >= fecha_inicio),
